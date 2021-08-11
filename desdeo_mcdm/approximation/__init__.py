47bf3df8e886220f2b07e0d3d0c07da08b1557eb
Stefu <stefan.s.otayagich@student.jyu.fi>
2021-08-11 11:55:22 +0300

    added init file

---
+++ desdeo_mcdm/approximation/__init__.py
@@ -0,0 +1,14 @@
+"""
+This module contains methods that approximate the Pareto front
+"""
+
+__all__ = [
+    "PAINT",
+    "PAINTException",
+]
+
+
+from desdeo_mcdm.approximation.PAINT import (
+    PAINT,
+    PAINTException,
+)
